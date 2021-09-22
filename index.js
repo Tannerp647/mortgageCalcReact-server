2c7e7500aaae2c28919fa38f600c5e1eb9cac359
Tanner Palmer <tannerp@ifit.com>
2021-09-22 12:46:44 -0600

    added mortgage calculations.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/hello", (req, res) => {
     console.log(req.query);
     res.send("Hello world");
 });
-
+``
 const testController = (req, res) => {
     const { loanTerm, loanAmount, interestRate } = req.body;
 
@@ -33,12 +33,59 @@ const testController = (req, res) => {
     }
 }
 
-const calculateMortgage = (req, res) => {
-    // do your mortgage calculations here
-    res.send('Placeholder')
+
+// /calculate-mortgage endpoint
+
+function numberWithCommas(x) {
+
+    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+
+
 };
 
+const calculateMortgage = (req, res) => {
+    // do your mortgage calculations here
+    const { loanTerm, loanAmount, interestRate } = req.body;
+    //make sure that there is a value 
+    if (!loanTerm || !loanAmount || !interestRate) {
+        res.status(400);
+        res.send("Missing entry")
+    };
+    // make sure that they are numbers
+    if (typeof loanTerm != "number" || typeof loanAmount != "number" || typeof interestRate != "number") {
+        res.status(400);
+        res.send("Invalid entry")
+    };
+    //all the calculations
+    const loanMonths = loanTerm * 12;
+    const interestRateDecimal = interestRate / 100 / 12;
+    const interestRatePlusOne = interestRateDecimal + 1;
+    const calcOne = 1 - Math.pow(interestRatePlusOne, - loanMonths);
+    const calcTwo = interestRateDecimal / calcOne;
+    const monthlyPayment = calcTwo * loanAmount;
+
+    //the output that will be displayed
+    try {
+        const response = {
+            "Monthly Payment": numberWithCommas(`$${Math.ceil((monthlyPayment))}`),
+            "Total Payment": numberWithCommas(`$${Math.ceil((monthlyPayment * loanMonths))}`),
+            "Total Interest": numberWithCommas(`$${Math.ceil(((monthlyPayment * loanMonths) - loanAmount))}`),
+            "Annual Payment": numberWithCommas(`$${Math.ceil((monthlyPayment * 12))}`),
+
+        };
+
+        res.send(response);
+    } catch (e) {
+        res.status(500);
+        res.send(e.message);
+
+    }
+
+};
+
+
 app.post("/test", testController);
+
 app.post("/calculate-mortgage", calculateMortgage);
 
 
@@ -52,9 +99,9 @@ app.listen(PORT, () => {
  * Read - GET
  * Update - PUT
  * Delete - DELETE
- * 
+ *
  * endpoints:
  * /hello -> returns hello world
  * /calculate-mortgage -> return mortgage stats
  * /hello?name=george
- */
+ **/
